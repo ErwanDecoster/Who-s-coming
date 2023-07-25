9d3bf40810caf6005bf66793359fc141d5489cb2
Decoster Erwan <wanerd2@gmail.com>
2023-07-25 12:20:56 +0200

    add of pages title and meta data

--- pages/events/[id].vue
+++ pages/events/[id].vue
@@ -152,5 +152,43 @@ export default {
 			navigateTo('/');
 		})
 	},
+	created() {
+		const metadata = {
+			desc: "Visualiser votre événement ajouter des invités, voyer qui de vos invités viendront.",
+			url: "http://localhost:3000/",
+			// url: "https://who-s-coming.vercel.app/",
+			pageName: "Événement - Who's coming",
+			imageDirectory: "cover.png"
+		}
+    	useHead({
+			title: metadata.pageName,
+			htmlAttrs: {
+				lang: 'fr'
+			},
+			meta: [
+				{ charset: 'utf-8' },
+				{ name: 'viewport', content: 'width=device-width, initial-scale=1' },
+				{ name: 'robots', content: 'index, follow'},
+				{ name: 'theme-color', content: '#014979'},
+				{ hid: 'description', name: 'description', content: metadata.desc },
+				{ property: 'og:url', content: metadata.url + this.$route.path },
+				{ property: 'og:type', content: 'article' },
+				{ property: 'og:title', content: metadata.desc },
+				{ property: 'og:description', content: metadata.desc },
+				{ property: 'og:image', content: metadata.url + metadata.imageDirectory },
+				{ property: 'twitter:card', content: 'summary_large_image' },
+				{ property: 'twitter:title', content: metadata.pageName },
+				{ property: 'twitter:description', content: metadata.desc },
+				{ property: 'twitter:image', content: metadata.url + metadata.imageDirectory },
+			],
+			link: [
+				{
+					hid: 'canonical',
+					rel: 'canonical',
+					href: metadata.url + this.$route.path,
+				},
+			],
+		})
+	}
 }
 </script>
